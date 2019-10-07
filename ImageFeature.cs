a85c0bcd003a46c764a96435345a3677607efe6c
aamir <wasi3aamir@gmail.com>
2019-10-08 02:35:08 +0600

    images feature will be added in this class

---
+++ ImageFeature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6ConsoleAppLibraryWebAPI
+{
+    public class ImageFeature
+    {
+        //images feature will be added in this class
+    }
+}
