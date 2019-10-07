2e48fdd44fb4e6cb570310c58eff877bbc55adc1
aamir <wasi3aamir@gmail.com>
2019-10-07 20:59:07 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment6ConsoleAppLibraryWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
